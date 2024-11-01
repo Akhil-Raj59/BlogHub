a0289d50d9103ec3800e9b09851a58fa3dc63f83
Asur-Daanav <542akhil@gmail.com>
2024-11-01 23:50:03 +0530

    deployment errors fixed

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,12 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
 
 const store = configureStore({
     reducer: {
-        auth : authSlice,
-        //TODO: add more slices here for posts
-    }
+        auth: authSlice,
+        // TODO: add more slices here for posts
+    },
+    devTools: process.env.NODE_ENV !== 'production', // Enable Redux DevTools only in development mode
 });
-
 
 export default store;
